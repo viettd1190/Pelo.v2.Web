7d9ab800424c606c455c7eea323e36719c400e23
tan tran <tantranvan9463@gmail.com>
2020-03-12 00:30:12 +0700

    update ward and user

--- src/Pelo.v2.Web/Controllers/UserController.cs
+++ src/Pelo.v2.Web/Controllers/UserController.cs
@@ -2,9 +2,11 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Pelo.Common.Dtos.User;
 using Pelo.v2.Web.Factories;
 using Pelo.v2.Web.Models.User;
 using Pelo.v2.Web.Services.User;
+using Pelo.Common.Extensions;
 
 namespace Pelo.v2.Web.Controllers
 {
@@ -46,6 +48,107 @@ namespace Pelo.v2.Web.Controllers
         {
             var result = await _userService.GetByPaging(model);
             return Json(result);
+        }
+
+        public async Task<IActionResult> Add()
+        {
+            var model = new UpdateUserModel();
+            await _baseModelFactory.PrepareBranches(model.AvaiableBranches);
+            await _baseModelFactory.PrepareRoles(model.AvaiableRoles);
+            await _baseModelFactory.PrepareDepartments(model.AvaiableDepartments);
+            model.AvaiableStatuses = new List<SelectListItem>
+                                           {
+                                                   new SelectListItem("Đã nghỉ việc",
+                                                                      "0"),
+                                                   new SelectListItem("Tất cả tình trạng",
+                                                                      "-1"),
+                                                   new SelectListItem("Vẫn còn làm",
+                                                                      "1",true)
+                                           };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(UpdateUserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _userService.Insert(new InsertUserRequest { IsActive = model.IsActive, BranchId = model.BranchId, DepartmentId = model.DepartmentId, Description = model.Description, DisplayName = model.DisplayName, Email = model.Email, FullName = model.FullName, Password = model.Password, PhoneNumber = model.PhoneNumber, RoleId = model.RoleId, Username = model.Username });
+                if (result.IsSuccess)
+                {
+                    TempData["Update"] = result.ToJson();
+                    return RedirectToAction("Index");
+                }
+
+                ModelState.AddModelError("", result.Message);
+            }
+            await _baseModelFactory.PrepareBranches(model.AvaiableBranches);
+            await _baseModelFactory.PrepareRoles(model.AvaiableRoles);
+            await _baseModelFactory.PrepareDepartments(model.AvaiableDepartments);
+            model.AvaiableStatuses = new List<SelectListItem>
+                                           {
+                                                   new SelectListItem("Đã nghỉ việc",
+                                                                      "0"),
+                                                   new SelectListItem("Tất cả tình trạng",
+                                                                      "-1"),
+                                                   new SelectListItem("Vẫn còn làm",
+                                                                      "1",true)
+                                           };
+            return View(model);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var result = await _userService.GetById(id);
+            if (result.IsSuccess)
+            {
+                var model = new UpdateUserModel();
+                await _baseModelFactory.PrepareBranches(model.AvaiableBranches);
+                await _baseModelFactory.PrepareRoles(model.AvaiableRoles);
+                await _baseModelFactory.PrepareDepartments(model.AvaiableDepartments);
+                model.AvaiableStatuses = new List<SelectListItem>
+                                           {
+                                                   new SelectListItem("Đã nghỉ việc",
+                                                                      "0"),
+                                                   new SelectListItem("Tất cả tình trạng",
+                                                                      "-1"),
+                                                   new SelectListItem("Vẫn còn làm",
+                                                                      "1",true)
+                                           };
+                model.Id = result.Data.Id;
+                model.IsActive = result.Data.IsActive; model.BranchId = result.Data.BranchId; model.DepartmentId = result.Data.DepartmentId; model.Description = result.Data.Description; model.DisplayName = result.Data.DisplayName; model.Email = result.Data.Email; model.FullName = result.Data.FullName; model.PhoneNumber = result.Data.PhoneNumber; model.RoleId = result.Data.RoleId;
+                return View(model);
+            }
+
+            return View("Notfound");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(UpdateUserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _userService.Update(new UpdateUserRequest { Id = model.Id, IsActive = model.IsActive, BranchId = model.BranchId, DepartmentId = model.DepartmentId, Description = model.Description, DisplayName = model.DisplayName, Email = model.Email, FullName = model.FullName, PhoneNumber = model.PhoneNumber, RoleId = model.RoleId });
+                if (result.IsSuccess)
+                {
+                    TempData["Update"] = result.ToJson();
+                    return RedirectToAction("Index");
+                }
+
+                ModelState.AddModelError("", result.Message);
+            }
+            await _baseModelFactory.PrepareBranches(model.AvaiableBranches);
+            await _baseModelFactory.PrepareRoles(model.AvaiableRoles);
+            await _baseModelFactory.PrepareDepartments(model.AvaiableDepartments);
+            model.AvaiableStatuses = new List<SelectListItem>
+                                           {
+                                                   new SelectListItem("Đã nghỉ việc",
+                                                                      "0"),
+                                                   new SelectListItem("Tất cả tình trạng",
+                                                                      "-1"),
+                                                   new SelectListItem("Vẫn còn làm",
+                                                                      "1",true)
+                                           };
+            return View(model);
         }
     }
 }
---
+++ src/Pelo.v2.Web/Models/User/UpdateUserModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+
+namespace Pelo.v2.Web.Models.User
+{
+    public class UpdateUserModel
+    {
+        public UpdateUserModel()
+        {
+            AvaiableBranches = new List<SelectListItem>();
+            AvaiableDepartments = new List<SelectListItem>();
+            AvaiableRoles = new List<SelectListItem>();
+            AvaiableStatuses = new List<SelectListItem>();
+        }
+
+        [JsonProperty("Id")]
+        public int Id { get; set; }
+
+        [JsonProperty("Code")]
+        public string Code { get; set; }
+
+        [JsonProperty("Username")]
+        public string Username { get; set; }
+
+        [JsonProperty("FullName")]
+        public string FullName { get; set; }
+
+        [JsonProperty("DisplayName")]
+        public string DisplayName { get; set; }
+
+        [JsonProperty("PhoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [JsonProperty("BranchId")]
+        public int BranchId { get; set; }
+
+        [JsonProperty("RoleId")]
+        public int RoleId { get; set; }
+
+        [JsonProperty("DepartmentId")]
+        public int DepartmentId { get; set; }
+
+        [JsonProperty("Email")]
+        public string Email { get; set; }
+
+        [JsonProperty("Avatar")]
+        public string Avatar { get; set; }
+
+        [JsonProperty("Description")]
+        public string Description { get; set; }
+
+        [JsonProperty("IsActive")]
+        public bool IsActive { get; set; }
+
+        [JsonProperty("Password")]
+        public string Password { get; set; }
+
+        [JsonProperty("ConfirmPassword")]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        public IList<SelectListItem> AvaiableBranches { get; set; }
+
+        public IList<SelectListItem> AvaiableDepartments { get; set; }
+
+        public IList<SelectListItem> AvaiableRoles { get; set; }
+
+        public IList<SelectListItem> AvaiableStatuses { get; set; }
+    }
+}
