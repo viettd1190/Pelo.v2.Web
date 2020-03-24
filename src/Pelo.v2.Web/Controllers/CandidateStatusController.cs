2bd277d019ef6eae1e008b5f2f51f034327015df
tan tran <tantv@vietmap.vn>
2020-03-24 23:16:38 +0700

    update tuyen dung

---
+++ src/Pelo.v2.Web/Controllers/CandidateStatusController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pelo.Common.Dtos.CandidateStatus;
+using Pelo.v2.Web.Models.CandidateStatus;
+using Pelo.v2.Web.Services.CandidateStatus;
+using Pelo.Common.Extensions;
+
+namespace Pelo.v2.Web.Controllers
+{
+    public class CandidateStatusController : Controller
+    {
+        private readonly ICandidateStatusService _candidateStatus;
+
+        public CandidateStatusController(ICandidateStatusService taskStatusService)
+        {
+            _candidateStatus = taskStatusService;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new CandidateStatusSearchModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetList(CandidateStatusSearchModel model)
+        {
+            var result = await _candidateStatus.GetByPaging(model);
+            return Json(result);
+        }
+
+        public async Task<IActionResult> Add()
+        {
+            return View(new CandidateStatusModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(CandidateStatusModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _candidateStatus.Insert(new InsertCandidateStatus { Color = model.Color, IsSendSms = model.IsSendSms, Name = model.Name, SmsContent = model.SmsContent, SortOrder = model.SortOrder });
+                if (result.IsSuccess)
+                {
+                    TempData["Update"] = result.ToJson();
+                    return RedirectToAction("Index");
+                }
+
+                ModelState.AddModelError("", result.Message);
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var model = await _candidateStatus.GetById(id);
+            if (model.IsSuccess)
+            {
+                return View(new CandidateStatusModel { Id = model.Data.Id, Name = model.Data.Name });
+            }
+
+            return View("Notfound");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(CandidateStatusModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _candidateStatus.Update(new UpdateCandidateStatus { Id = model.Id, Color = model.Color, IsSendSms = model.IsSendSms, Name = model.Name, SmsContent = model.SmsContent, SortOrder = model.SortOrder });
+                if (result.IsSuccess)
+                {
+                    TempData["Update"] = result.ToJson();
+                    return RedirectToAction("Index");
+                }
+
+                ModelState.AddModelError("", result.Message);
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _candidateStatus.Delete(id);
+            return Json(result);
+        }
+    }
+}
--- src/Pelo.v2.Web/Services/CandidateStatus/CandidateStatusService.cs
+++ src/Pelo.v2.Web/Services/CandidateStatus/CandidateStatusService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -17,6 +18,12 @@ namespace Pelo.v2.Web.Services.CandidateStatus
         Task<CandidateStatusListModel> GetByPaging(CandidateStatusSearchModel request);
 
         Task<TResponse<bool>> Delete(int id);
+
+        Task<IEnumerable<CandidateStatusSimpleModel>> GetAll();
+
+        Task<TResponse<GetCandidateStatusResponse>> GetById(int id);
+        Task<TResponse<bool>> Update(UpdateCandidateStatus model);
+        Task<TResponse<bool>> Insert(InsertCandidateStatus model);
     }
 
     public class CandidateStatusService : BaseService,
@@ -94,6 +101,92 @@ namespace Pelo.v2.Web.Services.CandidateStatus
                                                             HttpMethod.Delete,
                                                             true);
                 if(response.IsSuccess)
+                {
+                    return await Ok(true);
+                }
+
+                return await Fail<bool>(response.Message);
+            }
+            catch (Exception exception)
+            {
+                return await Fail<bool>(exception);
+            }
+        }
+
+        public async Task<IEnumerable<CandidateStatusSimpleModel>> GetAll()
+        {
+            try
+            {
+                var response = await HttpService.Send<IEnumerable<CandidateStatusSimpleModel>>(ApiUrl.CANDIDATE_STATUS_GET_ALL,
+                                                                                    null,
+                                                                                    HttpMethod.Get,
+                                                                                    true);
+
+                if (response.IsSuccess)
+                    return response.Data;
+
+                throw new PeloException(response.Message);
+            }
+            catch (Exception exception)
+            {
+                throw new PeloException(exception.Message);
+            }
+        }
+
+        public async Task<TResponse<GetCandidateStatusResponse>> GetById(int id)
+        {
+            try
+            {
+                var url = string.Format(ApiUrl.CANDIDATE_STATUS_GET_BY_ID, id);
+                var response = await HttpService.Send<GetCandidateStatusResponse>(url,
+                                                                      null,
+                                                                      HttpMethod.Get,
+                                                                      true);
+                if (response.IsSuccess)
+                {
+                    return await Ok(response.Data);
+                }
+
+                return await Fail<GetCandidateStatusResponse>(response.Message);
+            }
+            catch (Exception exception)
+            {
+                return await Fail<GetCandidateStatusResponse>(exception);
+            }
+        }
+
+        public async Task<TResponse<bool>> Update(UpdateCandidateStatus model)
+        {
+            try
+            {
+                var url = ApiUrl.CANDIDATE_STATUS_UPDATE;
+                var response = await HttpService.Send<bool>(url,
+                                                            model,
+                                                            HttpMethod.Put,
+                                                            true);
+                if (response.IsSuccess)
+                {
+                    return await Ok(true);
+                }
+
+                return await Fail<bool>(response.Message);
+            }
+            catch (Exception exception)
+            {
+                return await Fail<bool>(exception);
+            }
+        }
+
+        public async Task<TResponse<bool>> Insert(InsertCandidateStatus model)
+        {
+            try
+            {
+                var url = ApiUrl.CANDIDATE_STATUS_UPDATE;
+                var response = await HttpService.Send<bool>(url,
+                                                           model,
+                                                            HttpMethod.Post,
+                                                            true);
+                if (response.IsSuccess)
                 {
                     return await Ok(true);
                 }
